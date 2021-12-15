5f643ec44a9ae3efaa2c20b166d02b2fc6c3eb40
Your Name <you@example.com>
2021-12-15 15:54:32 +0530

    changing

--- mark14.js
+++ mark14.js
@@ -19,17 +19,17 @@ function calculateProfitAndLoss(initial, quantity, current) {
         var lossPercentage = (loss / initial) * 100;
 
         showOutput(
-            `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+            `the loss is ${loss} and the percent is ${lossPercentage}%`
         );
     } else if (current > initial) {
         var profit = (current - initial) * quantity;
         var profitPercentage = (profit / initial) * 100;
 
         showOutput(
-            `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+            ` he profit is ${profit} and the percent is ${profitPercentage}%`
         );
     } else {
-        showOutput(`No pain no gain and no gain no pain`);
+        showOutput(`No gain no loss`);
     }
 }
 
